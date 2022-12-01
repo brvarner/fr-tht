bcf0de0932cf5e96c9050983d74927d1ef7bbfb8
brvarner <bvarner4592@gmail.com>
2022-12-01 16:48:33 -0600

    updated documentation in component index

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 // You'll find an index file in the root of the helpers, containers, and components folders.
 // These files allow us access to every export we need with a neater import syntax.
 
+// Each of these components is built with Styled Components, which allows us to use
+// different subcomponents for each one, instead of creating new components for every function.
 export { default as Form } from "./form";
 export { default as Dropdown } from "./dropdown";
 export { default as Spacer } from "./spacer";
